c53f95a68642debe922f5c33f2f78df6cb1cdb0d
NOEL318 <noelrinaya318@gmail.com>
2026-04-10 17:18:32 -0600

    Initial Commit

---
+++ MyBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBuddy
+//
+//  Created by Noel Rincón on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBuddy/MyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBuddyApp.swift
+//  MyBuddy
+//
+//  Created by Noel Rincón on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
